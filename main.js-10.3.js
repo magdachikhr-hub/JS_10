0707198b5989919657c7b920b7fb667416f8a6a0
magda chikhradz <magda292005@gmail.com>
2026-04-07 16:53:00 +0400

    switch

---
+++ main.js-10.3.js
@@ -0,0 +1,40 @@
+//js_12
+
+function sum() {
+  console.log("hello");
+}
+
+sum();
+
+function sum2(name, age) {
+  console.log(`${name} is ${age} years old`);
+}
+
+sum2("magda", 20);
+
+//
+
+//switch
+
+let month = 1;
+
+switch (month) {
+  case 1:
+    console.log("ianvari");
+    break;
+
+  case 2:
+    console.log("tebervali");
+    break;
+
+  case 3:
+    console.log("marti");
+    break;
+
+  default:
+    console.log("danarcheni tveebia");
+}
+
+// case "ianvari":  console.log("axali celia")
+//break
+

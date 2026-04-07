033f0a5963b51ecbfaa210ccfead01c35c9c7ded
magda chikhradz <magda292005@gmail.com>
2026-04-07 17:49:19 +0400

    cycle

--- main.js-10.3.js
+++ main.js-10.3.js
@@ -47,3 +47,22 @@ switch (month) {
 for (let i = 0; i < 5; i++) {
   console.log("magda");
 }
+
+//for (let i = 0; i <= 5; i++) {
+// console.log("magda");
+//}
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// i++ - eshveba bolos
+
+//
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}

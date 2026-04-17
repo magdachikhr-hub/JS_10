1e8f11636721b7b70234352cdcb8391a41b852fc
magda chikhradze <magda292005@gmail.com>
2026-04-17 15:09:50 +0400

    pyramid patterns with loops

--- arrays.js
+++ arrays.js
@@ -14,3 +14,47 @@
 //unshift: adds an element to the beginning of an array
 
 //splice: adds or removes elements from an array - not an string method
+
+//
+
+//strings are immutable in js, we cannot change the value of an original string, we can only create a new string with the desired changes.
+let text = "some placeholder text ";
+
+text.slice(4, 10);
+text.toUpperCase();
+
+console.log(text);
+
+//arrays are mutable in js, we can change the value of an array, we can add, remove or change the elements of an array.
+
+//array methods:
+//push: adds an element to the end of an array
+let arr = [23, 456, 678, "some text"];
+
+arr.push("georgia", "usa");
+console.log(arr);
+
+//pop: removes the last element of an array
+let newArr = [13, 56, 234];
+newArr.pop();
+console.log(newArr);
+
+//shift: removes the first element of an array
+let cities = ["Tbilisi", "Gurdjaani", "Batumi"];
+cities.shift();
+console.log(cities);
+//unshift: adds an element to the beginning of an array
+let fruits = ["apple", "banana", "pineapple"];
+fruits.unshift("berry");
+console.log(fruits);
+//splice: adds or removes elements from an array - not an string method
+
+let vegetables = ["potatoes", "tomato", "carrot", "cabbage"];
+
+//chaamatet tavshi cucumber
+
+//boloshi moakelit bolo elementi
+
+//tavshic moakelit bolo elementi
+
+//boloshi miamatet greens, beans
--- loops.js
+++ loops.js
@@ -11,3 +11,38 @@ console.log(arr[3]);
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i] + 3);
 }
+
+// let arr = [23, 456, 678, 'some text']
+
+// arr.push('georgia', 'usa')
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+// let newArr = [13, 56, 234]
+// newArr.pop()
+// console.log(newArr)
+
+// for(){
+
+// }
+
+let pyramid = "";
+
+// for(let i = 1; i < 10; i++){
+
+//     for(let j = 0; j < i; j++){
+//         pyramid +='*'
+//     }
+//     pyramid += '\n'
+// }
+// console.log(pyramid)
+
+for (let i = 1; i < 10; i++) {
+  for (let j = 10; j > i; j--) {
+    pyramid += "*";
+  }
+  pyramid += "\n";
+}
+console.log(pyramid);

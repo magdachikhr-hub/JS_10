cc970a59325c1a91afd45cf19bea902dfc80e5b3
magda chikhradze <magda292005@gmail.com>
2026-04-17 15:06:49 +0400

    simple arrays

---
+++ arrays.js
@@ -0,0 +1,16 @@
+//arrays.js
+
+//strings are immutable in js, we cannot change the value of a string, we can only create a new string with the desired changes.
+
+//arrays are mutable in js, we can change the value of an array, we can add, remove or change the elements of an array.
+
+//array methods:
+//push: adds an element to the end of an array
+
+//pop: removes the last element of an array
+
+//shift: removes the first element of an array
+
+//unshift: adds an element to the beginning of an array
+
+//splice: adds or removes elements from an array - not an string method
---
+++ loops.js
@@ -0,0 +1,13 @@
+// loops.js
+
+let arr = [234, 345, 4];
+
+// let arr2 = [34, 'sdf', 'sdfj', 47, [364, 35], {city: 'Tbilisi', age: 5}]
+
+arr.push("new");
+
+console.log(arr[3]);
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i] + 3);
+}

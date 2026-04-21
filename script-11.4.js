0e9b5ed1bbeb56bd57ca383885fb5e6e5bde8ed2
Magda Chikhradze <magda292005@gmail.com>
2026-04-21 19:02:15 +0400

    simple or composed

--- script-11.4.js
+++ script-11.4.js
@@ -18,3 +18,29 @@ function findLongestWord(sentence) {
 findLongestWord("quick brown fox jumpsss on a tree");
 findLongestWord("hello i love javascript");
 findLongestWord("one two three four five ten seventeen");
+
+//
+
+let number = 25;
+
+function simpleOrComposed(num) {
+  if (num <= 0) {
+    console.log("arc martivia arc shedgenili");
+  } else {
+    let multiple = 0;
+    for (let i = 1; i < 25; i++) {
+      if (num % i === 0) {
+        multiple++;
+      }
+    }
+    console.log(multiple);
+    if (multiple > 2) {
+      console.log("shedgenilia");
+    } else {
+      console.log("martivia");
+    }
+  }
+}
+
+simpleOrComposed(9);
+simpleOrComposed(23);
